c08450a4be4dddc09e385918f1e9b19c624ac8cc
Alissa <alissa.paluska@gmail.com>
2016-04-25 14:25:01 -0700

    add Picasso

--- app/src/main/java/epicodus/localrestaurants/RestaurantListAdapter.java
+++ app/src/main/java/epicodus/localrestaurants/RestaurantListAdapter.java
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
 
 import butterknife.Bind;
@@ -57,6 +59,7 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
         }
 
         public void bindRestaurant(Restaurant restaurant) {
+            Picasso.with(mContext).load(restaurant.getImageUrl()).into(mRestaurantImageView);
             mNameTextView.setText(restaurant.getName());
             mCategoryTextView.setText(restaurant.getCategories().get(0));
             mRatingTextView.setText("Rating: " + restaurant.getRating() + "/5");
