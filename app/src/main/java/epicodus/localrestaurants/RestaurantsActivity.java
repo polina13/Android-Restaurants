dbbb4d42d507f7aa4bae9ace8320b1391cf111b6
Alissa <alissa.paluska@gmail.com>
2016-04-25 14:23:25 -0700

    add recyclerView to yelpResuts on RestaurantsActivity

--- app/src/main/java/epicodus/localrestaurants/RestaurantsActivity.java
+++ app/src/main/java/epicodus/localrestaurants/RestaurantsActivity.java
@@ -3,10 +3,8 @@ package epicodus.localrestaurants;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -18,8 +16,9 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class RestaurantsActivity extends AppCompatActivity {
-    private static final String TAG = RestaurantsActivity.class.getSimpleName();
-    @Bind(R.id.listView) ListView mListView;
+    public static final String TAG = RestaurantsActivity.class.getSimpleName();
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+    private RestaurantListAdapter mAdapter;
 
     public ArrayList<Restaurant> mRestaurants = new ArrayList<>();
 
@@ -31,14 +30,14 @@ public class RestaurantsActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
+
         getRestaurants(location);
     }
 
     private void getRestaurants(String location) {
         final YelpService yelpService = new YelpService();
 
-
-        yelpService.findRestaurants(location, new Callback() {
+        YelpService.findRestaurants(location, new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
                 e.printStackTrace();
@@ -49,26 +48,14 @@ public class RestaurantsActivity extends AppCompatActivity {
                 mRestaurants = yelpService.processResults(response);
 
                 RestaurantsActivity.this.runOnUiThread(new Runnable() {
-
                     @Override
                     public void run() {
-                        String[] restaurantNames = new String[mRestaurants.size()];
-                        for (int i = 0; i < restaurantNames.length; i++) {
-                            restaurantNames[i] = mRestaurants.get(i).getName();
-                        }
-
-                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(RestaurantsActivity.this, android.R.layout.simple_list_item_1, restaurantNames);
-                        mListView.setAdapter(adapter);
-
-                        for (Restaurant restaurant : mRestaurants) {
-                            Log.d(TAG, "Name: " + restaurant.getName());
-                            Log.d(TAG, "Phone: " + restaurant.getPhone());
-                            Log.d(TAG, "Website: " + restaurant.getWebsite());
-                            Log.d(TAG, "Image url: " + restaurant.getImageUrl());
-                            Log.d(TAG, "rating: " + Double.toString(restaurant.getRating()));
-                            Log.d(TAG, "Address: " + android.text.TextUtils.join("," ,restaurant.getAddress()));
-                            Log.d(TAG, "Categories: " + restaurant.getCategories().toString());
-                        }
+                        mAdapter = new RestaurantListAdapter(getApplicationContext(), mRestaurants);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager =
+                                new LinearLayoutManager(RestaurantsActivity.this);
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
                     }
                 });
             }
