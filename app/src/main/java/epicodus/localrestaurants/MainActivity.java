669482952d3a06f6c5a8908323b155c2ef269f07
Alissa <alissa.paluska@gmail.com>
2016-05-02 10:53:12 -0700

    add firebase reading data, errors with null object if databse is empty for savedLocations

--- app/src/main/java/epicodus/localrestaurants/MainActivity.java
+++ app/src/main/java/epicodus/localrestaurants/MainActivity.java
@@ -5,21 +5,31 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     public static final String TAG = MainActivity.class.getSimpleName();
+    private Firebase mSearchedLocationRef;
+    private ValueEventListener mSearchedLocationRefListener;
+
+
+//    private SharedPreferences mSharedPreferences;
+//    private SharedPreferences.Editor mEditor;
+
+
     @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
     @Bind(R.id.locationEditText) EditText mLocationEditText;
-    private SharedPreferences mSharedPreferences;
-    private SharedPreferences.Editor mEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,30 +37,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        mEditor = mSharedPreferences.edit();
+//        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+//        mEditor = mSharedPreferences.edit();
 
         mFindRestaurantsButton.setOnClickListener(this);
+        mSearchedLocationRef = new Firebase(Constants.FIREBASE_URL_SEARCHED_LOCATION);
+
+        mSearchedLocationRefListener = mSearchedLocationRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String locations = dataSnapshot.getValue().toString();
+                Log.d("Location updated", locations);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        mSearchedLocationRef.removeEventListener(mSearchedLocationRefListener);
     }
 
     @Override
     public void onClick(View v) {
         if (v == mFindRestaurantsButton) {
             String location = mLocationEditText.getText().toString();
+            // if(!(location).equals("")) {
+            //   addToSharedPreferences(location);
+            // }
             saveLocationToFirebase(location);
-             if(!(location).equals("")) {
-//                   addToSharedPreferences(location);
-                 Intent intent = new Intent(MainActivity.this, RestaurantListActivity.class);
-                 intent.putExtra("location", location);
-                 startActivity(intent);
-             }
+            Intent intent = new Intent(MainActivity.this, RestaurantListActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
         }
     }
+
+    // private void addToSharedPreferences(String location) {
+    //    mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
+    //  }
+
     public void saveLocationToFirebase(String location) {
         Firebase searchedLocationRef = new Firebase(Constants.FIREBASE_URL_SEARCHED_LOCATION);
         searchedLocationRef.push().setValue(location);
     }
-    // private void addToSharedPreferences(String location) {
-    //    mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
-    //  }
 }
