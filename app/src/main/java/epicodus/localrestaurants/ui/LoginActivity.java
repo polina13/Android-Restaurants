188fe5deab23dda02427b0ffe2541f18a2b40c67
polina <polina.mihova@yahoo.com>
2016-05-04 12:02:26 -0700

    Add user authentication and check for errorrs

--- app/src/main/java/epicodus/localrestaurants/ui/LoginActivity.java
+++ app/src/main/java/epicodus/localrestaurants/ui/LoginActivity.java
@@ -5,12 +5,16 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -31,10 +35,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
-        mRegisterTextView.setOnClickListener(this);
+
         mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(LoginActivity.this);
         mSharedPreferencesEditor = mSharedPreferences.edit();
         mFirebaseRef = new Firebase(Constants.FIREBASE_URL);
+
+        mRegisterTextView.setOnClickListener(this);
         mPasswordLoginButton.setOnClickListener(this);
     }
 
@@ -60,5 +66,45 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         if(password.equals("")) {
             mPasswordEditText.setError("Password cannot be blank");
         }
+
+        mFirebaseRef.authWithPassword(email, password, new Firebase.AuthResultHandler() {
+            @Override
+                public void onAuthenticated(AuthData authData) {
+                if (authData != null) {
+                    String userUid = authData.getUid();
+
+                    String userInfo = authData.toString();
+                    Log.d("TAG", "Currently logged in: " + userInfo);
+
+                    mSharedPreferencesEditor.putString(Constants.KEY_UID, userUid).apply();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+             }
+    }
+
+            @Override
+            public void onAuthenticationError(FirebaseError firebaseError) {
+                switch (firebaseError.getCode()) {
+                    case FirebaseError.INVALID_EMAIL:
+                    case FirebaseError.USER_DOES_NOT_EXIST:
+                    mEmailEditText.setError("Please check that you entered your email correctly");
+                        break;
+                    case FirebaseError.INVALID_PASSWORD:
+                        mEmailEditText.setError(firebaseError.getMessage());
+                        break;
+                    case  FirebaseError.NETWORK_ERROR:
+                        showErrorToast("There was a problem with the network connection");
+                        break;
+                    default:
+                        showErrorToast(firebaseError.toString());
+
+                }
+            }
+            private void showErrorToast(String message) {
+                Toast.makeText(LoginActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+    });
     }
 }
