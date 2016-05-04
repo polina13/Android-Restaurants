16f59e35fca33511ddf308448ee40085e7b113a5
polina <polina.mihova@yahoo.com>
2016-05-04 09:54:09 -0700

    Add templates for create account and login

---
+++ app/src/main/java/epicodus/localrestaurants/ui/LoginActivity.java
@@ -0,0 +1,15 @@
+package epicodus.localrestaurants.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import epicodus.localrestaurants.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
